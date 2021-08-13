e3107f1ecdda848bfa4e55d914acdd3e04d40238
jenyas <jenyas@1touch.io>
2021-08-12 22:12:28 -0400

    update package version

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/client',
     '<rootDir>/apps/server',
     '<rootDir>/libs/client/sign-in-view',
